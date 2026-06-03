54256f62b2cdec6a989bccde6ea4a60189f95ca6
irsnwhydi <wahyudiirsan51@gmail.com>
2026-06-03 22:29:22 +0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://fitpred-production.up.railway.app';
+const API_BASE_URL = 'https://fitpred-production.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
